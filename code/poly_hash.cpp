11bc10afd25d7192bbec762796e720f6ded3eeed
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2021-04-28 11:22:45 +0200

    updated polyhash

--- code/poly_hash.cpp
+++ code/poly_hash.cpp
@@ -3,15 +3,14 @@ ll P = 12233720368547789LL;
 ll B = 260;
 struct PolyHash {
 	vector<ll> hashes, ex;
-	PolyHash(const string& s) : hashes(s.size()), ex(s.size() + 1) {
-		hashes[0] = s[0] + 1; ex[0] = 1; ex[1] = B;
-		for (size_t i = 1; i < s.size(); i++) {
-			hashes[i] = ((hashes[i - 1] * B) % P + s[i] + 1) % P;
+	PolyHash(const string& s) : hashes(s.size() + 1), ex(s.size() + 1) {
+		hashes[0] = 1; ex[0] = 1; ex[1] = B;
+		for (size_t i = 0; i < s.size(); i++) {
+			hashes[i + 1] = ((hashes[i] * B) % P + s[i] + 1) % P;
 			ex[i + 1] = (ex[i] * B) % P;
 		}
 	}
 	ll hash(ll lo, ll hi) {
-		if (lo == 0) return hashes[hi];
-		return (hashes[hi] - ((lll)hashes[lo - 1] * ex[hi - lo]) % P + P) % P;
+		return ((lll)hashes[hi] - (lll)hashes[lo] * (lll)ex[hi - lo] % P + P) % P;
 	}
 };
