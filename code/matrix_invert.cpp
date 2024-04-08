d07e8360d7289043e7a28d88a87c48926274eb5b
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2024-04-08 20:14:55 +0200

    added some two column layout

--- code/matrix_invert.cpp
+++ code/matrix_invert.cpp
@@ -14,15 +14,19 @@ ll matInv(vector<vector<double>>& A) {
 					r = j, c = k;
 		if (fabs(A[r][c]) < 1e-12) return i;
 		A[i].swap(A[r]); tmp[i].swap(tmp[r]);
-		for (ll j = 0; j < n; j++)
-			swap(A[j][i], A[j][c]), swap(tmp[j][i], tmp[j][c]);
+		for (ll j = 0; j < n; j++) {
+			swap(A[j][i], A[j][c]);
+			swap(tmp[j][i], tmp[j][c]);
+		}
 		swap(col[i], col[c]);
 		double v = A[i][i];
 		for (ll j = i+1; j < n; j++) {
 			double f = A[j][i] / v;
 			A[j][i] = 0;
-			for (ll k = i+1; k < n; k++) A[j][k] -= f*A[i][k];
-			for (ll k = 0; k < n; k++) tmp[j][k] -= f*tmp[i][k];
+			for (ll k = i+1; k < n; k++)
+				A[j][k] -= f*A[i][k];
+			for (ll k = 0; k < n; k++)
+				tmp[j][k] -= f*tmp[i][k];
 		}
 		for (ll j = i+1; j < n; j++) A[i][j] /= v;
 		for (ll j = 0; j < n; j++) tmp[i][j] /= v;
@@ -31,7 +35,8 @@ ll matInv(vector<vector<double>>& A) {
 	for (ll i = n-1; i > 0; --i) {
 		for (ll j = 0; j < i; j++) {
 			double v = A[j][i];
-			for (ll k = 0; k < n; k++) tmp[j][k] -= v*tmp[i][k];
+			for (ll k = 0; k < n; k++)
+				tmp[j][k] -= v*tmp[i][k];
 		}
 	}
 	for (ll i = 0; i < n; i++)
